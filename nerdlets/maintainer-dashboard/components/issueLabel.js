26b65587f3f317d1b10bf0e63874e65b690a8c10
Noah Koontz <nkoontz@newrelic.com>
2020-10-28 12:09:34 -0700

    docs: add some missing comments

--- nerdlets/maintainer-dashboard/components/issueLabel.js
+++ nerdlets/maintainer-dashboard/components/issueLabel.js
@@ -2,6 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { BlockText } from 'nr1';
 
+/**
+ * Default GitHub label colors, pulled by creating a repository. Used to correct
+ * for some slightly different color shades caused by a GitHub color transition
+ * awhile ago.
+ */
 export const KNOWN_LABEL_COLORS = new Map([
   ['bug', 'd73a4a'],
   ['documentation', '0075ca'],
@@ -16,7 +21,18 @@ export const KNOWN_LABEL_COLORS = new Map([
   ['repolinter', 'fbca04'],
 ]);
 
-// stolen from https://stackoverflow.com/questions/3942878/how-to-decide-font-color-in-white-or-black-depending-on-background-color
+/**
+ * Function to automatically select light or dark text based off of the
+ * background color the text will sit on. Heavily inspired by
+ * https://stackoverflow.com/questions/3942878/how-to-decide-font-color-in-whit
+ * -or-black-depending-on-background-color
+ *
+ * @param {string} bgColor The background color text will sit on, in 6 character
+ *     hex format.
+ * @param {any} lightColor The light option to use for text color.
+ * @param {any} darkColor The dark option to use for text color.
+ * @returns {any} Either lightColor or darkColor depending on bgColor's shade.
+ */
 function pickTextColorBasedOnBgColor(bgColor, lightColor, darkColor) {
   const color = bgColor.charAt(0) === '#' ? bgColor.substring(1, 7) : bgColor;
   const r = parseInt(color.substring(0, 2), 16); // hexToR
@@ -25,15 +41,21 @@ function pickTextColorBasedOnBgColor(bgColor, lightColor, darkColor) {
   return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? darkColor : lightColor;
 }
 
+/**
+ * A component to imitate a GitHub Issue/PR label with NR styling. Inspired by
+ * primer/css and some inspect elementing.
+ *
+ * This component will automatically adjust label colors for known default
+ * labels (in `KNOWN_LABEL_COLORS`) to prevent slightly different label colors
+ * with the same name from creating visual confusion.
+ */
 export default class IssueLabel extends React.PureComponent {
   static propTypes = {
+    /** The label name (ex. "bug") */
     name: PropTypes.string.isRequired,
+    /** The label color in hex format */
     color: PropTypes.string.isRequired,
   };
-
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     const bgColor = KNOWN_LABEL_COLORS.has(this.props.name)
